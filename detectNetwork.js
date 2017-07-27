6eb3a5cb4eb6a02e72d382b9492d8a3a6d6f59b9
arsimr16 <arsimr16@g.holycross.edu>
2017-07-27 16:14:19 -0400

    write detectNetwork function to distinguish card networks

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,9 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if (/^3[89]\d{12}$/.test(cardNumber)) {
+  	return 'Diner\'s Club';
+  } else if (/^3[47]\d{13}$/.test(cardNumber)) {
+  	return 'American Express';
+  }
 };
-
-
