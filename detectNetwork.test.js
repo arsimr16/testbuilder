4354c4d60d26d7730830fe171eb4d4a1e8d559bb
arsimr16 <arsimr16@g.holycross.edu>
2017-07-28 17:19:56 -0400

    clean up all tests for step three

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -1,49 +1,3 @@
-/*
- * You'll eventually be given instructions how to use this file
- * If you want to use it before then, you'll have to figure it out yourself
- */
-
-// You don't actually want to fill *this* value in on line 9, but you'll see
-// other places in this file where you'll replace the FILL_ME_IN with a
-// different value.
-var FILL_ME_IN = 'Fill this value in';
- /*
-describe('Introduction to Mocha Tests - READ ME FIRST', function() {
-  // A Mocha test is just a function!
-  // If the function throws an error when run, it fails.
-  // If it doesn't throw an error when run, it doesn't fail. 
-  // To read more about mocha, visit mochajs.org
-
-  // Once you've read and understood this section, please comment it out. 
-  // You will not be able to proceed with a failing test. 
-  
-  it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
-  });
-  
-  it('Doesn\'t throw an error, so it doesn\'t fail', function() {
-    // This test doesn't really test anything at all! It will pass no matter what.
-    var even = function(num){
-      return num/2 === 0;
-    }
-    return even(10) === true;
-  });
-  
-
-  // In tests, we want to compare the expected behavior to the actual behavior.
-  // A test should only fail if the expected behavior doesn't match the actual.
-  it('Throws an error when expected behavior does not match actual behavior', function() {
-    var even = function(num){
-      return num/2 === 0;
-    }
-
-    if(even(10) !== true) {
-      throw new Error('10 should be even!');
-    }
-  });
-});
-*/
-
 function getAllCombos(prefixes, lengths) {
   var allCombos = [];
   prefixes.forEach(function(prefix) {
@@ -59,25 +13,7 @@ function getAllCombos(prefixes, lengths) {
 }
 
 describe('Diner\'s Club', function() {
-  // Be careful, tests can have bugs too...
-  var assert = function(isTrue) {
-    if(!isTrue) {
-      throw new Error('Test failed');
-    }
-  };
-
-  it('has a prefix of 38 and a length of 14', function() {
-    //throw new Error('Delete me!');
-    if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
-      throw new Error('Test failed');
-    }
-  });
-
-  it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('39345678901234') !== 'Diner\'s Club') {
-      throw new Error('Test failed');
-    }
-  });
+  var assert = chai.assert;
   
   it('has a prefix of 38 or 39 and a length of 14', function() {
     var allCombos = getAllCombos(['38', '39'], ['14']);
@@ -89,22 +25,7 @@ describe('Diner\'s Club', function() {
 });
 
 describe('American Express', function() {
-  // It can get annoying to keep typing the if/throw, so here is a
-  // helper function to throw an error if the input statement isn't true. 
-  var assert = function(isTrue) {
-    if(!isTrue) {
-      throw new Error('Test failed');
-    }
- 
-  };
-
-  it('has a prefix of 34 and a length of 15', function() {
-    assert(detectNetwork('343456789012345') === 'American Express');
-  });
-
-  it('has a prefix of 37 and a length of 15', function() {
-    assert(detectNetwork('373456789012345') === 'American Express');
-  });
+  var assert = chai.assert;
 
   it('has a prefix of 34 or 37 and a length of 15', function() {
     var allCombos = getAllCombos(['34', '37'], ['15']);
@@ -115,24 +36,7 @@ describe('American Express', function() {
 });
 
 describe('Visa', function() {
-  // Chai is an entire library of helper functions for tests!
-  // Chai provides an assert that acts the same as our previous assert.
-  // Search the documentation to figure out how to access it. 
-  //   http://chaijs.com/
   var assert = chai.assert;
- 
-
-  it('has a prefix of 4 and a length of 13', function() {
-    assert(detectNetwork('4123456789012') === 'Visa');
-  });
-
-  it('has a prefix of 4 and a length of 16', function() {
-    assert(detectNetwork('4123456789012345') === 'Visa');
-  });
-
-  it('has a prefix of 4 and a length of 19', function() {
-    assert(detectNetwork('4123456789012345678') === 'Visa');
-  });
 
   it('has a prefix of 4 and a length of 13, 16, or 19', function() {
     var allCombos = getAllCombos(['4'], ['13', '16', '19']);
@@ -143,42 +47,7 @@ describe('Visa', function() {
 });
 
 describe('MasterCard', function() {
-  // Chai lets you write more human-readable tests that throw helpful errors.
-  // Expect syntax is one way to do this, but there are others. 
-  // If you want to know more, check out the documentation. 
-  //   http://chaijs.com/api/bdd/
-  var expect = chai.expect;
   var assert = chai.assert;
- 
-  it('has a prefix of 51 and a length of 16', function() {
-    expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
-  });
- 
-  it('has a prefix of 52 and a length of 16', function() {
-    expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
-  });
- 
-  it('has a prefix of 53 and a length of 16', function() {
-    expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
-  });
- 
-
-  // You can also use should instead of expect, which changes the style
-  // slightly. It really doesn't matter which one you use - check out 
-  // http://chaijs.com/guide/styles/ for more info, but it's important
-  // to be consistent (unlike in this file, where we use BOTH expect
-  // and should, but that's just for learning), so once you've gotten 
-  // these tests to pass using should syntax, refactor your tests to 
-  // use either expect or should, but not both. 
-  //var should = chai.should();
-  
-  it('has a prefix of 54 and a length of 16', function() {
-    expect(detectNetwork('5412345678901234')).to.equal('MasterCard');
-  });
- 
-  it('has a prefix of 55 and a length of 16', function() {
-    expect(detectNetwork('5512345678901234')).to.equal('MasterCard');
-  });
 
   it('has a prefix of 51-55 and a length of 16', function() {
     var allCombos = getAllCombos(['51', '52', '53', '54', '55'], ['16']);
@@ -189,38 +58,7 @@ describe('MasterCard', function() {
 });
 
 describe('Discover', function() {
-  // Tests without a function will be marked as "pending" and not run
-  // Implement these tests (and others) and make them pass!
-  var expect = chai.expect;
   var assert = chai.assert;
-
-  it('has a prefix of 6011 and a length of 16', function() {
-    expect(detectNetwork('6011567890123456')).to.equal('Discover');
-  });
-
-  it('has a prefix of 6011 and a length of 19', function() {
-    expect(detectNetwork('6011567890123456789')).to.equal('Discover');
-  });
-
-  for (var prefix = 644; prefix <= 649; prefix++) {
-    prefix = prefix.toString();
-    (function(prefix) {
-      it('has a prefix of ' + prefix + ' and a length of 16', function() {
-        expect(detectNetwork(prefix + '4567890123456')).to.equal('Discover');
-      });
-      it('has a prefix of ' + prefix + ' and a length of 19', function() {
-        expect(detectNetwork(prefix + '4567890123456789')).to.equal('Discover');
-      });
-    })(prefix)
-  }
-
-  it('has a prefix of 65 and a length of 16', function() {
-    expect(detectNetwork('6534567890123456')).to.equal('Discover');
-  });
-
-  it('has a prefix of 65 and a length of 19', function() {
-    expect(detectNetwork('6534567890123456789')).to.equal('Discover');
-  });
 
   it('has a prefix of 6011, 644-649, or 65 and a length of 16 or 19', function() {
     var allCombos = getAllCombos(['6011', '644', '645', '646', '647', '648', '649', '65'], ['16', '19']);
@@ -231,50 +69,9 @@ describe('Discover', function() {
 });
 
 describe('Maestro', function() {
-  // Write full test coverage for the Maestro card
-  var expect = chai.expect;
   var assert = chai.assert;
-/*
-  for (var length = 12; length <= 19; length++) {
-    length = length.toString();
-    var remainingDigits;
-    if (length === '12') {
-      remainingDigits = '56789012';
-    } else if (length === '13') {
-      remainingDigits = '567890123';
-    } else if (length === '14') {
-      remainingDigits = '5678901234';
-    } else if (length === '15') {
-      remainingDigits = '56789012345';
-    } else if (length === '16') {
-      remainingDigits = '567890123456';
-    } else if (length === '17') {
-      remainingDigits = '5678901234567';
-    } else if (length === '18') {
-      remainingDigits = '56789012345678';
-    } else if (length === '19') {
-      remainingDigits = '567890123456789';
-    }
-    (function(length) {
-      it('has a prefix of 5018 and length of ' + length, function() {
-        expect(detectNetwork('5018' + remainingDigits)).to.equal('Maestro');
-      });
 
-      it('has a prefix of 5020 and a length of ' + length, function() {
-        expect(detectNetwork('5020' + remainingDigits)).to.equal('Maestro');
-      });
-
-      it('has a prefix of 5038 and a length of ' + length, function() {
-        expect(detectNetwork('5038' + remainingDigits)).to.equal('Maestro');
-      });
-
-      it('has a prefix of 6304 and a length of ' + length, function() {
-        expect(detectNetwork('6304' + remainingDigits)).to.equal('Maestro');
-      });
-    })(length)
-  }
-*/
-  it('has a prefix of 5018, 5020, 5036, or 6304 and a length of 12-19', function() {
+  it('has a prefix of 5018, 5020, 5038, or 6304 and a length of 12-19', function() {
     var allCombos = getAllCombos(['5018', '5020', '5038', '6304'], ['12', '13', '14', '15', '16', '17', '18', '19']);
     allCombos.forEach(function(combo) {
       assert(detectNetwork(combo) === 'Maestro');
