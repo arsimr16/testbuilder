45f2250661697bfa38eedc0934cdefb61d31e495
arsimr16 <arsimr16@g.holycross.edu>
2017-07-31 20:57:30 -0400

    pass all tests for testbuilder exercise

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -17,6 +17,8 @@ function getAllNumsInRange(nums) {
 
 function getAllCombos(network, prefixes, lengths) {
   var assert = chai.assert;
+  prefixes = getAllNumsInRange(prefixes);
+  lengths = getAllNumsInRange(lengths);
   var allCombos = [];
   prefixes.forEach(function(prefix) {
     lengths.forEach(function(length) {
@@ -34,7 +36,7 @@ function getAllCombos(network, prefixes, lengths) {
 
 describe('Diner\'s Club', function() {
   it('has a prefix of 38 or 39 and a length of 14', function() {
-    getAllCombos('Diner\'s Club', ['38', '39'], ['14']);
+    getAllCombos('Diner\'s Club', ['38-39'], ['14']);
   });
 });
 
@@ -52,21 +54,63 @@ describe('Visa', function() {
 
 describe('MasterCard', function() {
   it('has a prefix of 51-55 and a length of 16', function() {
-    getAllCombos('MasterCard', ['51', '52', '53', '54', '55'], ['16'])
+    getAllCombos('MasterCard', ['51-55'], ['16']);
   });
 });
 
 describe('Discover', function() {
   it('has a prefix of 6011, 644-649, or 65 and a length of 16 or 19', function() {
-    getAllCombos('Discover', ['6011', '644', '645', '646', '647', '648', '649', '65'], ['16', '19']);
+    getAllCombos('Discover', ['6011', '644-649', '65'], ['16', '19']);
   });
 });
 
 describe('Maestro', function() {
   it('has a prefix of 5018, 5020, 5038, or 6304 and a length of 12-19', function() {
-    getAllCombos('Maestro', ['5018', '5020', '5038', '6304'], ['12', '13', '14', '15', '16', '17', '18', '19'])
+    getAllCombos('Maestro', ['5018', '5020', '5038', '6304'], ['12-19']);
   });
 });
 
-describe('should support China UnionPay')
-describe('should support Switch')
+describe('China UnionPay', function() {
+  it('has a prefix of 624-626, or 6282-6288 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['624-626', '6282-6288'], ['16-19']);
+  });
+
+  it('has a prefix of 622126-622226 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622126-622226'], ['16-19']);
+  });
+
+  it('has a prefix of 622227-622327 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622227-622327'], ['16-19']);
+  });
+
+  it('has a prefix of 622328-622428 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622328-622428'], ['16-19']);
+  });
+
+  it('has a prefix of 622429-622529 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622429-622529'], ['16-19']);
+  });
+
+  it('has a prefix of 622530-622630 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622530-622630'], ['16-19']);
+  });
+
+  it('has a prefix of 622631-622731 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622631-622731'], ['16-19']);
+  });
+
+  it('has a prefix of 622732-622832 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622732-622832'], ['16-19']);
+  });
+
+  it('has a prefix of 622833-622925 and a length of 16-19', function() {
+    getAllCombos('China UnionPay', ['622833-622925'], ['16-19']);
+  });
+});
+
+
+describe('Switch', function() {
+  it('has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19', function() {
+    getAllCombos('Switch', ['4903', '4905', '4911', '4936', '564182', '633110', '6333', '6759'], ['16', '18-19']);
+  });
+});
